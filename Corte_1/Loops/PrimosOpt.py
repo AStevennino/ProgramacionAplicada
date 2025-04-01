0751db7bd3dfcb64618784e222ee637f511a7f11
AStevennino <137360132+AStevennino@users.noreply.github.com>
2025-04-01 16:17:00 -0500

    Add files via upload

---
+++ Corte_1/Loops/PrimosOpt.py
@@ -0,0 +1,96 @@
+# codigo para encontrar numeros primos
+tope_rango=30
+n=0
+primo = True
+while (n < tope_rango):
+    for div in range (2, n):
+        if (n % div == 0):
+            primo = False
+    if (primo):
+        print(n)
+    else: 
+        primo = True
+        n += 1
+
+# codigo para numeros primos optimizado
+
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        if (n % div == 0):
+            primo = False
+            break
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+
+# codigo para conocer la medida de la optimizacion
+ciclos_sin_break = 0
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        ciclos_sin_break += 1
+        if (n % div == 0):
+            primo = False
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_sin_break))
+
+
+ciclos_con_break = 0
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        ciclos_con_break += 1
+        if (n % div == 0):
+            primo = False
+            break
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_con_break))
+print('Se optimizó a un ' + str(ciclos_con_break/ciclos_sin_break) + '% de ciclos aplicando break')
+
+# optimizacion con mayor rango
+tope_rango=100
+ciclos_sin_break = 0
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        ciclos_sin_break += 1
+        if (n % div == 0):
+            primo = False
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_sin_break))
+
+ciclos_con_break = 0
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        ciclos_con_break += 1
+        if (n % div == 0):
+            primo = False
+            break
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_con_break))
+print('Se optimizó a un ' + str(ciclos_con_break/ciclos_sin_break) + '% de ciclos aplicando break')
---
+++ Corte_1/Loops/WhileLoop.py
@@ -0,0 +1,7 @@
+for i in range(1,5):
+    while 1 <=4:
+        i += 1
+        print(i)
+
+        break
+    
